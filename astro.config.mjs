076cfedc28c4bab8d8d4ef7d669322fc2443509d
Mathieu Woiry <mathieu.woiry@gmail.com>
2024-05-22 10:47:08 +0200

    edited config for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://m-w1995.github.io',
+  base: 'pizzalova',
   integrations: [preact(), tailwind()]
 });
